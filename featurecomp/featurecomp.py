b5bf632610ef5060e19868765cebb4dc421b86e2
vishnuvarthan M <88725911+Vishnuvarthan007@users.noreply.github.com>
2023-11-18 10:45:08 +0530

    Update featurecomp.py

--- featurecomp/featurecomp.py
+++ featurecomp/featurecomp.py
@@ -2,17 +2,31 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 from PIL import Image
+
+
+
 ch = {
     'chart': ['bar', 'line', 'scatter', 'box', 'histogram', 'pie'],
 }
+
 ch = pd.DataFrame(ch)
-st.title("EV Comparisons")
-df = pd.read_csv("featurecomp/Cheapestelectriccars-EVDatabase.csv")
+
+st.title("EV car Comparisons")
+df = pd.read_csv("Cheapestelectriccars-EVDatabase.csv")
+sliced_df = df.drop('Name', axis=1)
+
+
+
 user_input_text_1 = st.multiselect("Select Car Types", df['Name'].unique())
-user_input_text_2 = st.selectbox("Select Parameter", df.columns)
+user_input_text_2 = st.selectbox("Select Parameter", sliced_df.columns)
 selected_chart = st.selectbox("Select Chart Type", ch['chart'].unique())
+
+
 if st.button("Generate chart"):
+    # Filter the DataFrame based on selected car types
     filtered_df = df[df['Name'].isin(user_input_text_1)]
+
+    # Create chart based on the selected type
     if selected_chart == 'histogram':
         fig = px.histogram(filtered_df, x=user_input_text_2, labels={'x': 'X-axis'}, title='Histogram comparison')
     else:
@@ -26,4 +40,6 @@ if st.button("Generate chart"):
             fig = px.box(filtered_df, x='Name', y=user_input_text_2, labels={'x': 'Car Type', 'y': 'Y-axis'}, title='Comparison')
         elif selected_chart == 'pie':
             fig = px.pie(filtered_df, names='Name', values=user_input_text_2, labels={'x': 'Car Type'}, title='Comparison')
+
+    # Display the chart
     st.plotly_chart(fig)
